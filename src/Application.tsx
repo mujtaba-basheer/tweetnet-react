19cbe29c02eb05909c70a1461c9948b64b181a6c
Mujtaba Basheer <mujtaba.fleapo@gmail.com>
2022-06-09 22:05:16 +0530

    First Commit

---
+++ src/Application.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+const Application = () => {
+  return (
+    <div>
+      <a
+        href="https://api.twitter.com/oauth/request_token"
+        rel="noreferrer"
+        target="_blank"
+      >
+        Log In
+      </a>
+    </div>
+  );
+};
+
+export default Application;
