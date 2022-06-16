7993f8eaf5f425f3cf8336913684be4f4c501fbd
Mujtaba Basheer <mujtaba.fleapo@gmail.com>
2022-06-16 12:35:23 +0530

    Added Reply To Tweet functionality

--- src/Tweets.tsx
+++ src/Tweets.tsx
@@ -12,6 +12,7 @@ type TokenObj = {
 type TweetObj = {
   id: string;
   text: string;
+  reply: boolean;
 };
 type TweetsApiResp = {
   status: boolean;
@@ -42,11 +43,22 @@ type RetweetApiResp = {
   };
 };
 
+type ReplyApiResp = {
+  status: boolean;
+  data: {
+    data: {
+      id: string;
+      text: string;
+    };
+  };
+};
+
 const Tweets = () => {
   const params = useParams();
   const navigate = useNavigate();
   const [token, setToken] = useState<string>("");
   const [tweets, setTweets] = useState<TweetObj[]>([]);
+  const [replyText, setReplyText] = useState<string>("");
 
   const getTweets = async (userId: string, token: string) => {
     if (!token) return null;
@@ -61,7 +73,7 @@ const Tweets = () => {
       );
 
       const { data } = (await req.json()) as TweetsApiResp;
-      setTweets(data.data);
+      setTweets(data.data.map((x) => ({ ...x, reply: false })));
     } catch (error) {
       console.error(error);
     }
@@ -107,6 +119,54 @@ const Tweets = () => {
     }
   };
 
+  const openReply = (_id: string) => {
+    setTweets((arr) =>
+      arr.map((x) => {
+        if (x.id === _id) x.reply = true;
+        else x.reply = false;
+        return x;
+      })
+    );
+    setReplyText("");
+  };
+
+  const closeReply = (_id: string) => {
+    setTweets((arr) =>
+      arr.map((x) => {
+        if (x.id === _id) x.reply = false;
+        return x;
+      })
+    );
+    setReplyText("");
+  };
+
+  const postReply = async (_id: string) => {
+    try {
+      const request = await fetch(`${process.env.REACT_APP_BASE_URL}/reply`, {
+        method: "POST",
+        headers: {
+          Authorization: token,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          tweet_id: _id,
+          text: replyText,
+        }),
+      });
+
+      const { status } = (await request.json()) as ReplyApiResp;
+      if (status) {
+        alert("Reply Posted Successfully!");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("Error Posting Reply :-/");
+    }
+
+    setTweets((arr) => arr.map((x) => ({ ...x, reply: false })));
+    setReplyText("");
+  };
+
   useEffect(() => {
     const tokenObj: TokenObj = JSON.parse(
       sessionStorage.getItem("token") || "null"
@@ -121,13 +181,28 @@ const Tweets = () => {
     <div className="tweets-page">
       <h1>Tweets</h1>
       <div className="tweets-grid">
-        {tweets.map(({ id, text }) => (
+        {tweets.map(({ id, text, reply }) => (
           <div className="tweets-grid__item" id={id}>
             <p>{text}</p>
             <div className="tweets-grid__item-bar">
               <button onClick={() => retweetTweet(id)}>Retweet</button>
               <button onClick={() => likeTweet(id)}>Like</button>
+              <button onClick={() => openReply(id)}>Reply</button>
             </div>
+            {reply && (
+              <div className="tweets-grid__item-reply">
+                <textarea
+                  value={replyText}
+                  onChange={(ev) => setReplyText(ev.target.value)}
+                />
+                <div className="tweets-grid__item-reply-bar">
+                  <button disabled={!replyText} onClick={() => postReply(id)}>
+                    Post Reply
+                  </button>
+                  <button onClick={() => closeReply(id)}>Cancel</button>
+                </div>
+              </div>
+            )}
           </div>
         ))}
       </div>
